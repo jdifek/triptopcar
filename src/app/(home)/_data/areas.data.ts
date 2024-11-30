e7da0c22ac81e6a9d7ede3aabebc11a3621c51ec
ilogush <ilogush@icloud.com>
2024-12-01 02:23:12 +0700

    Update areas.data.ts

--- src/app/(home)/_data/areas.data.ts
+++ src/app/(home)/_data/areas.data.ts
@@ -2,13 +2,73 @@ import { Area } from "@/typing/interfaces";
 
 export const areas: Area[] = [
   {
-    deliveryPrice: 10,
+    deliveryPrice: 600,
     id: 1,
-    name: "Tashkent",
+    name: "Airport Phuket",
   },
   {
-    deliveryPrice: 10,
+    deliveryPrice: 800,
     id: 2,
-    name: "123",
+    name: "Mai Khao Beach",
+  },
+  {
+    deliveryPrice: 600,
+    id: 3,
+    name: "Naiyang Beach",
+  },
+  {
+    deliveryPrice: 500,
+    id: 4,
+    name: "Nai Thon Beach",
+  },
+  {
+    deliveryPrice: 500,
+    id: 5,
+    name: "Bang Thao Beach",
+  },
+  {
+    deliveryPrice: 400,
+    id: 6,
+    name: "Kamala Beach",
+  },
+  {
+    deliveryPrice: 400,
+    id: 7,
+    name: "Patong Beach",
+  },
+  {
+    deliveryPrice: 300,
+    id: 8,
+    name: "Karon Beach",
+  },
+  {
+    deliveryPrice: 300,
+    id: 9,
+    name: "Kata Beach",
+  },
+  {
+    deliveryPrice: 400,
+    id: 10,
+    name: "Rawai Beach",
+  },
+  {
+    deliveryPrice: 300,
+    id: 11,
+    name: "Thalang",
+  },
+  {
+    deliveryPrice: 200,
+    id: 12,
+    name: "Katu",
+  },
+  {
+    deliveryPrice: 200,
+    id: 13,
+    name: "Chalong",
+  },
+  {
+    deliveryPrice: 400,
+    id: 14,
+    name: "Panwa",
   },
 ];
