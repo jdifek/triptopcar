73de5a0033e4c9230864a2fcb0f2e9338dbfd93d
jdifek <givotkov11@gmail.com>
2024-12-04 15:01:43 +0200

    fdsfs

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    turboMode: false, // Отключение Turbopack
-  },
+
 };
 
 export default nextConfig;
